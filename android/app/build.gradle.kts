38920e7fc80ed13dceb65616e798e8e34a3a80bd
Jainish Jain <jainishjain.1105@gmail.com>
2026-03-03 20:20:17 +0530

    Update library

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -23,7 +23,7 @@ android {
 
     defaultConfig {
         applicationId = "com.jainish.face_detection_app"
-        minSdk = 21
+        minSdk = 23
         targetSdk = 35
         versionCode = flutter.versionCode
         versionName = flutter.versionName
